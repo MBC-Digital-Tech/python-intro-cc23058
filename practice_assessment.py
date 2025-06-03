acd3d32c4f646ed0503f541ca0f3f51e0082a6e4
cc23058 <cc23058@mbc.school.nz>
2025-06-04 11:02:33 +1200

    need to fix - practice

--- practice_assessment.py
+++ practice_assessment.py
@@ -1,16 +1,19 @@
 print("Welcome to the Dog Breed Selector!")
 name = input("What is your name? ")
 print("Available dog breeds:")
-breeds_list = ["1. Labrador", "2. German Shepherd","3. Beagle","4. Bulldog", "5. Poodle"]
-for breed in breeds_list:
-    print(breed)
-breed = int(input("Enter the number of your prefferred dog breed: "))
+breeds_list = [" ", "Labrador", "German Shepherd","Beagle","Bulldog", "Poodle"]
+
+for breeds in breeds_list:
+    print(breeds)
+breeds = int(input("Enter the number of your prefferred dog breed: "))
 age = int(input ("Enter the preffered age of dog (1-15):"))
 print(f'Thank you, {name}!')
-print(f'You prefer {breeds_list[breed]} that is about {age} years old.')
+print(f'You prefer {breeds_list[breeds]} that is about {age} years old.')
 print("Good luck finding your perfect furry friend!")
 
-if breed > 5:
+if breeds > 5:
     print('Invalid selection please select a number between 1-5')
 if age > 15:
     print("Invalid age please select a number between 1-15")
+elif age < 1:
+    print("Invalid age please select a valid age")
